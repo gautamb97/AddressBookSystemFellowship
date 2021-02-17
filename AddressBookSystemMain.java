706abb93ae6024a2305224e7768942be06e777a1
gautamb97 <www.gautam971997@gmail.com>
2021-02-17 19:33:11 +0530

    Refactored:Added options for users

--- AddressBookSystemMain.java
+++ AddressBookSystemMain.java
@@ -1,9 +1,27 @@
 import java.util.*;
+
 public class AddressBookSystemMain {
 	public static Scanner personContact = new Scanner(System.in);
-	Contact contact = new Contact("firstName","lastName","address","city","state","email address",0,0);
+	Contact contact = new Contact("firstName", "lastName", "address", "city", "state", "email address", 0, 0);
 	List<Object> listOfContact = new ArrayList<>();
 	
+	public void takingOption() {
+		System.out.println("Enter your option: ");
+		System.out.println("1.Add Contact");
+		System.out.println("2.Display Contacts");
+		int option = personContact.nextInt();
+		switch(option) {
+		case 1:
+			addingContact();
+			break;
+		case 2:
+			displayContact();
+			break;
+		default:
+			break;
+		}
+		takingOption();
+	}
 	public void addingContact() {
 		contact.creatingContact();
 		contact.getFirstName();
@@ -16,16 +34,16 @@ public class AddressBookSystemMain {
 		contact.getPhoneNumber();
 		listOfContact.add(contact);
 	}
+
 	
 	public void displayContact() {
-		addingContact();
 		System.out.println(listOfContact);
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to address book system");
 		AddressBookSystemMain contactDetails = new AddressBookSystemMain();
-		contactDetails.displayContact();
+		contactDetails.takingOption();
 	}
 
 }
